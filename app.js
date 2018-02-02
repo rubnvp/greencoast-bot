865a3f64d141f96f5cf2cb76d512a4dffb9a89ee
rubnvp <rubn.vp@gmail.com>
2018-02-02 13:36:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const { FB_ID, FB_SECRET, SLACK_TOKEN } = require('./secrets');
+
+axios
+    .get('https://graph.facebook.com/v2.12/oauth/access_token', {
+        params: {
+            client_id: FB_ID,
+            client_secret: FB_SECRET,
+            grant_type: 'client_credentials',
+        }
+    })
+    .then(response => response.data.access_token) // get fb accessToken
+    .then(accessToken => axios
+    .get('https://graph.facebook.com/v2.12/828823737185056/feed', {
+        params: {
+            access_token: accessToken,
+            limit: 1,
+        }
+    }))
+    .then(response => response.data.data[0]) // get lastPost
+    .then(lastPost => axios
+    .get(`https://slack.com/api/chat.postMessage`, {
+        params: {
+            token: SLACK_TOKEN,
+            channel: 'U2XE9BDRA',//"#greencoast", U2XE9BDRA: Ruben
+            text: ':robot_face: ' + lastPost.message,
+        }
+    }))
+    .catch(error => {
+        console.log(error);
+    });
